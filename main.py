5424c6957ae57c3fc225ccd8fb00e5615a26700e
Jonas-ML <jonas0104@live.dk>
2024-04-18 19:05:29 +0200

    _Fixed Json parsing in main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
 from apiClient import *
+import json
 
 
-a = restClient()
+
+
+headers = {
+    'Authorization': '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',
+    'Accept' : '*/*',
+}
+## "?sha=dev" is branch definition
+r = requests.get("https://api.github.com/repos/Jonas-ML/afgangsProjekt/commits?sha=dev", headers=headers)
+
+if r.status_code == 200:
+    response_json = r.json()
+    json_response = json.dumps(response_json, indent=2)
+    print(json_response)
+else:
+    print("Error", r.status_code)
