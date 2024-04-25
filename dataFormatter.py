8736bd13203686e8d8b11fd303f0ff1d5463bb24
Jonas-ML <jonas0104@live.dk>
2024-04-25 14:01:18 +0200

    _Updated sequence diagram to include data Formatter, _Added main windows in gui on main

--- dataFormatter.py
+++ dataFormatter.py
@@ -1,5 +1,15 @@
 from main import *
-import json
+
+a = restClient()
+
+response, status_code = a.get("repos/Jonas-ML/afgangsProjekt/commits", params={"sha":"dev"})
+
+if status_code == 200:
+    res = a.formatResponse(response)
+else:
+    print("ERROR CODE:", status_code)
+    
+
 
 data = json.loads(res)
 search = "message"
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
 from apiClient import *
 import json
+from tkinter import *
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk
 
-a = restClient()
+root = Tk()
+root.title("Dashboard")
+root.configure(background="white")
+root.minsize(200, 200)
+root.maxsize(1920, 1080)
+root.geometry("1200x400+50+50")
+tabsystem = ttk.Notebook(root)
 
-response, status_code = a.get("repos/Jonas-ML/afgangsProjekt/commits", params={"sha":"dev"})
 
-if status_code == 200:
-    res = a.formatResponse(response)
-else:
-    print("ERROR CODE:", status_code)
-    
+tab1 = Frame(tabsystem)
+tabsystem.add(tab1, text="!H")
+tabsystem.pack(expand=1, fill="both")
+
+
+
+
+root.mainloop()
