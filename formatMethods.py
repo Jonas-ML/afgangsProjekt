83edd8cbfedde01047faf7e7a35e5ce7022c10ad
Jonas-ML <jonas0104@live.dk>
2024-05-31 16:32:16 +0200

    added Exception handling in dataformatter

--- formatMethods.py
+++ formatMethods.py
@@ -5,29 +5,46 @@ class dataFormatter:
     def __init__(self):
         pass
         
-    def fetchRepos(self, response): # Indexes the json object to extract repo names
-        data = json.loads(response)
-        repo_options = [repo['name'] for repo in data]
-        return repo_options    
-    
-    def formatResponse(self, response): # Formats json string, to json object structure
-        res = response.json()
-        json_str = json.dumps(res, indent=2)
-        return json_str
+    def fetchRepos(self, response): 
+        try:
+            data = json.loads(response)
+            repo_options = [repo['name'] for repo in data]
+            return repo_options 
+        except json.JSONDecodeError as e: # Syntax errors, missing contents, unsupported data types
+            print(f"An error occurred while decoding JSON: {e}")
+        except Exception as e:
+            print(f"An error occurred while fetching repos: {e}")
 
-    def formatCommits(self, response, choosen_repo, choosen_Branch): # 
-        data = json.loads(response)
-        commit_details = ""
-        for commit in data:
-            commit_message = commit['commit']['message']
-            commit_date = commit['commit']['committer']['date']
-            commit_details += (
-                 "============================================================================\n"
-                f"Repository: {choosen_repo} on Branch: {choosen_Branch}\n"
-                f"Commit: {commit_message}\n"
-                f"Date:   {commit_date}\n"
-            )
-        return commit_details
+    
+    def formatResponse(self, response):
+        try:
+            res = response.json()
+            json_str = json.dumps(res, indent=2)
+            return json_str
+        except Exception as e:
+            print(f"An error occurred while formatting the response: {e}")
+
+
+    def formatCommits(self, response, choosen_repo, choosen_Branch):
+        try:
+            data = json.loads(response)
+            commit_details = ""
+            for commit in data:
+                commit_message = commit['commit']['message']
+                commit_date = commit['commit']['committer']['date']
+                commit_details += (
+                    "============================================================================\n"
+                    f"Repository: {choosen_repo} on Branch: {choosen_Branch}\n"
+                    f"Commit: {commit_message}\n"
+                    f"Date:   {commit_date}\n"
+                )
+            return commit_details
+        except json.JSONDecodeError as e: # Syntax errors, missing contents, unsupported data types
+            print(f"An error occurred while decoding JSON: {e}")
+        except KeyError as e: # If you try to find a key that doesnt exist
+            print(f"Missing key in JSON data: {e}")
+        except Exception as e:
+            print(f"An error occurred while formatting commits: {e}")
 
     def textFormatting(self, txt, keywords):
         try:
